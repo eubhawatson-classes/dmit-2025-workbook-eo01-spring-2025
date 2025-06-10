a7cf1a370cc396787b4b5c0f0b46d7c85b306039
Valerie Watson <vwatson@nait.ca>
2025-06-09 19:54:24 -0600

    Completes Lesson 11 (Valitron OOP Library).

--- module-2/11-php-validation-library/index.php
+++ module-2/11-php-validation-library/index.php
@@ -29,8 +29,46 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
     // This line creates a new Validator object (that's the `new` keyword) and gives it the data we want to validate.
     $v = new Validator($data);
 
-    
-}
+    // Next, let's define some of the basic validation rules we want to use. 
+
+    // For the 'required' rule, each of the listed fields must be present in the input data and cannot be empty (not null, not an empty string, not just whitespace).
+    $v->rule('required', ['name', 'email', 'gender', 'country']);
+
+    // The value of the `email` field must conform to a standard email format. Valitron uses PHP's `filter_var($value, FILTER_VALIDATE_EMAIL)` under the hood.
+    $v->rule('email', 'email');
+
+    // The submitted gender/country must exactly match one of the listed values. If the user submits anything else, validation fails. 
+    $v->rule('in', 'gender', ['female', 'male', 'other']);
+    $v->rule('in', 'country', ['au', 'ca', 'gb', 'us']);
+
+    // The message's string length can be no longer than 256 characters. If it exceeds this, it will trigger a validation error in that field.
+    $v->rule('lengthMax', 'message', 256);
+
+    // Even with our shiny new library, we may still need to write some custom checks. Let's start by seeing if the user chose a hobby.
+    if (!empty($data['hobbies'])) {
+        foreach($data['hobbies'] as $hobby) {
+            if (!in_array($hobby, ['reading', 'traveling', 'gaming'])) {
+                $v->error('hobbies', 'Hobbies contains an invalid value. Please select from the provided options.');
+                break;
+            }
+        }
+    } else { // if the user did not select at least one hobby
+        $v->error('hobbies', 'At least one hobby must be selected.');
+    }
+
+    // Up above, we defined all of our rules (or the things we want to check for when we validate). Now, let's actually validate! This condition first of all executes validation; after that's finished, it also checks to see if there were any errors. 
+    if ( $v->validate() && empty( $v->errors() ) ) {
+        // After validation, if there are no errors, the user passes. 
+        $pass_message = "<p>Form submitted successfully!</p>";
+
+        // Because the user stays on the same page as the form, we may want to wipe out their form values rather than risk them spam-submitting the form data. 
+        $data = [];
+    } else {
+        // However, if there are errors, we'll assign it to a variable here to echo it out to the user.
+        $validation_errors = $v->errors();
+    }
+
+} // end of 'if the user hit submit'
 
 ?>
 
@@ -50,17 +88,50 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
     <section class="row">  
         <h1>Contact Us</h1>
 
+        <!-- This block prints out any validation messages to the user. -->
+         <?php if ($validation_errors) : ?>
+            <div class="alert alert-danger">
+                <p>There were <strong>validation errors</strong> in your submission:</p>
+                <ul>
+                    <?php
+                        /* 
+                            Our validation errors are stored in a multi-dimensional array. 
+
+                            In the first level, we have each field (ex. name, email, gender, etc.) and its errors.
+
+                            In the second level, we have each individual error message.
+
+                            So, for example, name may have multiple error messages. This means that we not only need to loop through the messages for each field, but also each individual error. 
+                        */
+                        foreach ($validation_errors as $field => $messages) {
+                            foreach ($messages as $warning) {
+                                echo "<li>$warning</li>";
+                            }
+                        }
+                    ?>
+                </ul>
+            </div>
+         <?php endif; 
+
+            if ($pass_message != "") {
+                echo "<div class=\"alert alert-success\">";
+                echo $pass_message;
+                echo "</div>";
+            }
+         
+         ?>
+
         <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
             <!-- Name -->
              <div class="mb-4">
                 <label for="name" class="form-label">Your Name:</label>
-                <input type="text" id="name" name="name" class="form-control">
+                <input type="text" id="name" name="name" class="form-control" value="<?= $data['name'] ?? ''; ?>">
              </div>
 
             <!-- Email -->
              <div class="mb-4">
                 <label for="email" class="form-label">Email Address:</label>
-                <input type="email" id="email" name="email" class="form-control">
+                <input type="email" id="email" name="email" class="form-control" value="<?= $data['email'] ?? ''; ?>">
              </div>
 
             <!-- Radio Buttons (Gender) -->
@@ -68,17 +139,17 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
                 <legend>Your Gender:</legend>
 
                 <div class="form-check">
-                    <input type="radio" id="female" value="female" name="gender" class="form-check-input">
+                    <input type="radio" id="female" value="female" name="gender" class="form-check-input" <?= (isset($data['gender']) == 'female') ? 'checked' : ''; ?>>
                     <label for="female" class="form-check-label">Female</label>
                 </div>
 
                 <div class="form-check">
-                    <input type="radio" id="male" value="male" name="gender" class="form-check-input">
+                    <input type="radio" id="male" value="male" name="gender" class="form-check-input" <?= (isset($data['gender']) == 'male') ? 'checked' : ''; ?>>
                     <label for="male" class="form-check-label">Male</label>
                 </div>
                 
                 <div class="form-check">
-                    <input type="radio" id="other" value="other" name="gender" class="form-check-input">
+                    <input type="radio" id="other" value="other" name="gender" class="form-check-input" <?= (isset($data['gender']) == 'other') ? 'checked' : ''; ?>>
                     <label for="other" class="form-check-label">Other</label>                    
                 </div>
              </fieldset>
@@ -88,15 +159,15 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
                 <legend>Hobbies:</legend>
 
                 <div class="form-check">
-                    <input type="checkbox" id="reading" name="hobbies" value="reading" class="form-check-input">
+                    <input type="checkbox" id="reading" name="hobbies" value="reading" class="form-check-input" <?= (isset($data['hobbies']) == 'reading') ? 'checked' : ''; ?>>
                     <label for="form-check-label" class="form-check-label">Reading</label>
                 </div>
                 <div class="form-check">
-                    <input type="checkbox" id="traveling" name="hobbies" value="traveling" class="form-check-input">
+                    <input type="checkbox" id="traveling" name="hobbies" value="traveling" class="form-check-input" <?= (isset($data['hobbies']) == 'traveling') ? 'checked' : ''; ?>>
                     <label for="form-check-label" class="form-check-label">Traveling</label>
                 </div>
                 <div class="form-check">
-                    <input type="checkbox" id="gaming" name="hobbies" value="gaming" class="form-check-input">
+                    <input type="checkbox" id="gaming" name="hobbies" value="gaming" class="form-check-input" <?= (isset($data['hobbies']) == 'gaming') ? 'checked' : ''; ?>>
                     <label for="form-check-label" class="form-check-label">Video Games</label>
                 </div>
             </fieldset>
@@ -105,18 +176,18 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
              <div class="mb-4">
                 <label for="country" class="form-label">Country:</label>
                 <select name="country" id="country" class="form-select">
-                    <option value="">-- Select Your Country --</option>
-                    <option value="au">Australia</option>
-                    <option value="ca">Canada</option>
-                    <option value="gb">United Kingdom</option>
-                    <option value="us">United States</option>
+                    <option value="" <?= (isset($data['country']) && $data['country'] == "") ? 'selected' : ''; ?>>-- Select Your Country --</option>
+                    <option value="au" <?= (isset($data['country']) && $data['country'] == "au") ? 'selected' : ''; ?>>Australia</option>
+                    <option value="ca" <?= (isset($data['country']) && $data['country'] == "ca") ? 'selected' : ''; ?>>Canada</option>
+                    <option value="gb" <?= (isset($data['country']) && $data['country'] == "gb") ? 'selected' : ''; ?>>United Kingdom</option>
+                    <option value="us" <?= (isset($data['country']) && $data['country'] == "us") ? 'selected' : ''; ?>>United States</option>
                 </select>
              </div>
 
             <!-- Text Area -->
              <div class="mb-3">
                 <label for="message" class="form-label">Message:</label>
-                <textarea name="message" id="message" maxlength="256" class="form-control"></textarea>
+                <textarea name="message" id="message" maxlength="256" class="form-control"><?= $data['message'] ?? ''; ?></textarea>
              </div>
 
             <!-- Submit -->
