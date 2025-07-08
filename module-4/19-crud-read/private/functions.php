0cfe0aaba432f41ddecfcf4e01d699f4ad9a73fe
Valerie Watson <vwatson@nait.ca>
2025-07-07 20:49:54 -0600

    Completes read and add pages in CRUD application.

--- module-4/19-crud-read/private/functions.php
+++ module-4/19-crud-read/private/functions.php
@@ -1,5 +1,7 @@
 <?php
 
+/* RESULTS TABLE */
+
 /**
  * This function fetches all of the cities in our database and prints them out in an HTML table. 
  * Note: We will add some parameters later on when we need it for our Edit and Delete pages.
@@ -21,14 +23,31 @@ function generate_table() {
              <tbody> \n";
 
         // Record Loop
+        foreach ($cities as $city) {
+            extract($city);
 
-        echo "</tbody>
-             </table>
-             <aside>
-             <h3 class=\"fs-5 fw-normal\">Notes</h3>
-             <p class=\"text-muted my-3\">&starf; indicates the capital of a province or territory.</p>
-             <p class=\"text-muted my-3\">Hover over <i class=\"bi bi-info-circle\" data-bs-toggle=\"tooltip\" title=\"I'm a tooltip!\"></i> to see additional trivia about the city.</p>
-             </aside>";
+            // If this city is a capital (i.e. if $is_capital has a TRUE value), we will print a filled star for the user.
+            $capital = ($is_capital) ? '&starf;' : '';
+
+            // `trivia ` is a nullable column, meaning there may or may not be a value there. We will check to see if there is anything; if there is, we'll give the user an info icon with a tooltip that they can rollover.
+            $trivia_info = ($trivia != NULL) ? "<i class=\"bi bi-info-circle\" data-bs-toggle=\"tooltip\" title=\"$trivia\"></i>" : '';
+
+            $population = number_format($population);
+
+            echo "<tr> \n
+                 <td>$capital $city_name, $province</td> \n
+                 <td>$population</td> \n
+                 <td>$trivia_info</td> \n
+                 </tr> \n";
+        }
+
+        echo "</tbody> \n
+             </table> \n
+             <aside> \n
+             <h3 class=\"fs-5 fw-normal\">Notes</h3> \n
+             <p class=\"text-muted my-3\">&starf; indicates the capital of a province or territory.</p> \n
+             <p class=\"text-muted my-3\">Hover over <i class=\"bi bi-info-circle\" data-bs-toggle=\"tooltip\" title=\"I'm a tooltip!\"></i> to see additional trivia about the city.</p> \n
+             </aside> \n";
     } else {
         echo "<h2 class=\"fw-light\">Oh no!</h2>";
         echo "<p>We're sorry, but we weren't able to find anything in our database.</p>";
---
+++ module-4/20-crud-add/public/includes/header.php
@@ -0,0 +1,47 @@
+<?php
+
+// Almost every page in this application will have a header, so we will include or require all of our other dependencies here.
+require_once dirname(__DIR__, 4) . '/data/connect.php';
+$connection = db_connect();
+
+include '../private/prepared.php';
+include '../private/functions.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title><?php echo $title; ?> | Canadian Cities Online Database</title>
+
+        <!-- BS CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <!-- BS Icons -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    </head>
+
+    <body class="min-vh-100 d-flex flex-column justify-content-between">
+        <header class="text-center p-3">
+            <nav class="mb-5">
+                <a href="index.php" class="btn btn-dark">Home</a>
+                <a href="add.php" class="btn btn-success">Add</a>
+                <a href="edit.php" class="btn btn-warning">Edit</a>
+                <a href="delete.php" class="btn btn-danger">Delete</a>
+            </nav>
+        </header>
+        <main class="container p-5">
+            <!-- Introduction -->
+            <section class="row justify-content-center text-center">
+                <div class="col col-md-10 col-xl-8">
+                    <h1 class="fw-light text-center"><?php echo $title; ?></h1>
+                    <p class="text-muted text-center lead mb-5"><?php echo $introduction; ?></p>
+                </div>
+            </section>
+            
+            <!-- Page Content -->
+            <section class="row justify-content-center">
+                <div class="col col-md-10 col-xl-8">
